01262ad7c91d67b22ed087ff5da77371f49599bb
Charles Raviaud <charles.raviaud@protonmail.com>
2021-09-02 23:04:53 +0200

    Editing info into components
    Replacing previous info by new ones into different components

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -8,8 +8,8 @@ export default function ContainerBlock({ children, ...customMeta }) {
   const router = useRouter();
 
   const meta = {
-    title: "Manu Arora - Developer, Writer, Creator and YouTuber",
-    description: `I've been developing websites for 5 years straight. Get in touch with me to know more.`,
+    title: "Charles Raviaud - Developer, Writer, Creator and YouTuber",
+    description: `I've been developing websites for 3 years straight. Get in touch with me to know more.`,
     image: "/avatar.png",
     type: "website",
     ...customMeta,
@@ -29,7 +29,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
           href={`https://yourwebsite.com${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Manu Arora" />
+        <meta property="og:site_name" content="Charles Raviaud" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
